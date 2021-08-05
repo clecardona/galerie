ee3a275c23f5a93aed6aba61b2a61dc01dc51a44
clecardona <clecardona@gmail.com>
2021-08-05 16:02:35 +0200

    btn active

--- src/components/GalleryComic.jsx
+++ src/components/GalleryComic.jsx
@@ -1,11 +1,20 @@
 import Card from "./Card";
+import { CSSTransitionGroup } from "react-transition-group";
 
 export default function GalleryComic({ images }) {
   return (
+              <CSSTransitionGroup
+                    transitionName="fade"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}
+                    transitionAppear={true}
+                    transitionAppearTimeout={400}
+                  >
     <div className="comic">
       {images.map((item) => {
         return <Card key={item.id} item={item} />;
       })}
     </div>
+      </CSSTransitionGroup>
   );
 }
--- src/components/GalleryTiles.jsx
+++ src/components/GalleryTiles.jsx
@@ -1,11 +1,20 @@
 import Card from "./Card";
+import { CSSTransitionGroup } from "react-transition-group";
 
 export default function GalleryTiles({ images }) {
   return (
+    <CSSTransitionGroup
+    transitionName="fade"
+    transitionEnterTimeout={500}
+    transitionLeaveTimeout={500}
+    transitionAppear={true}
+    transitionAppearTimeout={500}
+  >
     <div className="tiles">
       {images.map((item) => {
         return <Card key={item.id} item={item} />;
       })}
     </div>
+    </CSSTransitionGroup>
   );
 }
